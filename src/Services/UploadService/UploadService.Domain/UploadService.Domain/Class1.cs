bf7e6b18cd5378c3c90b9d95df915bc4737ad5d1
ARC-373 <alancardoso.arc@gmail.com>
2026-04-03 14:17:54 -0300

    Upload base structure

---
+++ src/Services/UploadService/UploadService.Domain/UploadService.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace UploadService.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ src/Services/UploadService/UploadService.Infrastructure/UploadService.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace UploadService.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
