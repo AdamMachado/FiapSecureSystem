dd891e483707b49958b079f810e1c74b5f55a38c
Alan dos Reis Cardoso <alan.cardoso@FDERIVS.com>
2026-04-27 17:20:23 -0300

    Fixed ProcessingService Dockerfile Included IntegrationEventMappers to ProcessingService.Application Included Aspire Dashboard Viewer to docker-compose

--- src/Services/ProcessingService/ProcessingService.Application/DependencyInjection.cs
+++ src/Services/ProcessingService/ProcessingService.Application/DependencyInjection.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using ProcessingService.Application.Abstractions.Messaging;
 using ProcessingService.Application.Integration.Consumed;
+using ProcessingService.Application.Integration.Published;
 using ProcessingService.Application.UseCases.CompleteAnalysisProcessing;
 using ProcessingService.Application.UseCases.FailAnalysisProcessing;
 using ProcessingService.Application.UseCases.GetProcessingResult;
 using ProcessingService.Application.UseCases.StartAnalysisProcessing;
+using ProcessingService.Domain.Events;
 using Shared.Contracts.IntegrationEvents;
 
 namespace ProcessingService.Application;
@@ -18,7 +20,21 @@ public static class DependencyInjection
         services.AddScoped<FailAnalysisProcessingHandler>();
         services.AddScoped<CompleteAnalysisProcessingHandler>();
 
-        services.AddScoped<IIntegrationEventHandler<AnalysisRequestedIntegrationEvent>, AnalysisRequestedMessageHandler>();
+        services.AddScoped<
+            IIntegrationEventHandler<AnalysisRequestedIntegrationEvent>,
+            AnalysisRequestedMessageHandler>();
+
+        services.AddScoped<
+            IIntegrationEventMapper<AnalysisProcessingStartedDomainEvent>,
+            AnalysisStartedIntegrationEventMapper>();
+
+        services.AddScoped<
+            IIntegrationEventMapper<AnalysisProcessingCompletedDomainEvent>,
+            AnalysisCompletedIntegrationEventMapper>();
+
+        services.AddScoped<
+            IIntegrationEventMapper<AnalysisProcessingFailedDomainEvent>,
+            AnalysisFailedIntegrationEventMapper>();
 
         return services;
     }
