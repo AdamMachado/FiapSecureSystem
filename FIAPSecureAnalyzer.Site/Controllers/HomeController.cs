07c439306581a0e25aeba6cd93b17f81e4801964
Adamm <Adamm@ADAM-PC>
2026-05-01 23:13:21 -0300

    Web

---
+++ FIAPSecureAnalyzer.Site/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using FIAPSecureAnalyzer.Site.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FIAPSecureAnalyzer.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
